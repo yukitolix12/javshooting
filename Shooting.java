17fdf706d07c152e0a2bde7ca4d87c9d5e878dc6
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-04 21:00:28 +0900

    create screen

---
+++ Shooting.java
@@ -0,0 +1,7 @@
+public class Shooting{
+    public static ShootingFrame shootingFrame;
+
+    public static void main(String[] args){
+        shootingFrame = new ShootingFrame();
+    }
+}
---
+++ ShootingFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class ShootingFrame extends JFrame {
+    public ShootingFrame(){
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setTitle("Shooting");
+        this.setSize(800, 800);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+    
+}
