185044a711fa5199fe79e28071a242b361885c36
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-05 10:53:55 +0900

    ウィンドウを小さくする

--- ShootingFrame.java
+++ ShootingFrame.java
@@ -4,7 +4,7 @@ public class ShootingFrame extends JFrame {
     public ShootingFrame(){
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setTitle("Shooting");
-        this.setSize(800, 800);
+        this.setSize(500, 500);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
