df68ec63fbe85d5381fff5c8f1cfb4818ab53bb2
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-05 11:01:40 +0900

    Graphicsをオーバーライドする

--- ShootingPanel.java
+++ ShootingPanel.java
@@ -1,5 +1,12 @@
 public class ShootingPanel extends JPanel{
+    public Graphics gra;
+    
     public ShootingPanel(){
         super();
+    }
+
+    //Override
+    public void paint(Graphics g){
+        super.paint(g);
     }
 }
