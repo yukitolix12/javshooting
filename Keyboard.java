db94c399fea37aed28f57c21c9535b95338d6c6a
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-07 16:32:51 +0900

    keyPressed、key動作作成

--- Keyboard.java
+++ Keyboard.java
@@ -1,5 +1,8 @@
 import java.awt.event.KeyAdapter;
 
 public class Keyboard extends KeyAdapter {
-    
+    //override
+    public void keyPressed(KeyEvent e){
+        super.keyPressed(e);
+    }
 }
